b512fd00eab0dc45faabccf4ce6803f66ada09ea
Apprentice <apprentice@devbootcamp.com>
2012-11-12 15:42:50 -0800

    Comment validation.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { redirect_to @comment.article }
+        format.html { redirect_to @comment.article, notice: 'something went wrong!' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,6 @@ class Comment < ActiveRecord::Base
   attr_accessible :body
 
   validates_associated :article
+
+  validates :body, :uniqueness => true
 end
